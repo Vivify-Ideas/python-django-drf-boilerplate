b7381fdb6358b72e3b15a7b93063ba4a744bd7b6
Milos Janjic <milosh012@gmail.com>
2019-09-06 13:41:04 +0200

    Added dotenv package.

--- manage.py
+++ manage.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appname.config")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+
+    dotenv.read_dotenv()
 
     try:
         from configurations.management import execute_from_command_line
@@ -18,7 +21,6 @@ if __name__ == "__main__":
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
-                "forget to activate a virtual environment?"
-            )
+                "forget to activate a virtual environment?")
         raise
     execute_from_command_line(sys.argv)
