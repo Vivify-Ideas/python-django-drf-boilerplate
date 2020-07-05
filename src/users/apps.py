3af31af18168fa39ce2a65de7a1695e9abf40f49
Nikola Spalevic <nikola.s@vivifyideas.com>
2020-07-05 20:01:21 +0200

    Activity Stream combined with notifications module

--- src/users/apps.py
+++ src/users/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'src.users'
+
+    # actstream register model
+    # def ready(self):
+    #     from actstream import registry
+    #     registry.register(self.get_model('User'))
