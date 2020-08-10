01481ba2d98bbd179b01954757218398658f2cbd
Milos Janjic <milosh012@gmail.com>
2020-08-10 16:26:06 +0200

    Fixing running test.

--- src/config/local.py
+++ src/config/local.py
@@ -1,18 +1,9 @@
-import os
 import sys
-
-from src.config.common import * # noqa
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from src.config.common import *  # noqa
 
 TESTING = sys.argv[1:2] == ['test']
 
-
 # Testing
-INSTALLED_APPS += ('django_nose', ) # noqa
+INSTALLED_APPS += ('django_nose', )  # noqa
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = [
-    BASE_DIR, '-s', '--nologcapture', '--with-coverage',
-    '--with-progressive', '--cover-package=src'
-]
+NOSE_ARGS = ['-s', '--nologcapture', '--with-progressive', '--with-fixture-bundling']
